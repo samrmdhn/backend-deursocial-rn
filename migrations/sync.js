845a9f8c7c3853aba93280079e1d099a08044395
ivannofick <ivannofick@gmail.com>
2024-11-05 17:05:19 +0700

    fix: fixed make visitor token

--- migrations/sync.js
+++ migrations/sync.js
@@ -1,22 +1,22 @@
 import db from '../configs/Database.js';
 import Example from '../apps/models/Example.js';
 import UsersModels from '../apps/models/UsersModels.js';
-import UsersActivityModels from '../apps/models/UsersActivityModels.js';
+import UsersAccessModels from '../apps/models/UsersAccessModels.js';
 
 
+// Menggunakan force: true akan membuat ulang tabel setiap kali dijalankan contoh => Example.sync({ force: true })
 async function syncDatabase() {
   try {
     await db.authenticate();
     console.log('Connection has been established successfully.');
 
-    // Sinkronisasi model dengan database
-    await Example.sync({ force: false }); // Menggunakan force: true akan membuat ulang tabel setiap kali dijalankan
-    console.log('Table table example has been created.');
+    // await Example.sync({ force: false }); 
+    // console.log('Table table example has been created.');
 
-    await UsersModels.sync({ force: false }); // Menggunakan force: true akan membuat ulang tabel setiap kali dijalankan
-    console.log('Table table Users has been created.');
+    // await UsersModels.sync({ force: false });
+    // console.log('Table table Users has been created.');
 
-    await UsersActivityModels.sync({ force: false }); // Menggunakan force: true akan membuat ulang tabel setiap kali dijalankan
+    await UsersAccessModels.sync({ force: false });
     console.log('Table table Users Activity has been created.');
 
     await db.close();
