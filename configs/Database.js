a0e227f3baf9f91676f15cc8a0575adcd8b006be
ivannofick <ivannofick@gmail.com>
2024-11-06 22:21:07 +0700

    fix: add pool on config databases

--- configs/Database.js
+++ configs/Database.js
@@ -1,10 +1,21 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config({ path: `./.env` });
-const db = new Sequelize(process.env.APP_DB_DATABASE, process.env.APP_DB_USERNAME, process.env.APP_DB_PASSWORD, {
-    host: process.env.APP_DB_HOST,
-    dialect: process.env.APP_DB_CONNECTION,
-    port: process.env.APP_DB_PORT
-});
+const db = new Sequelize(
+    process.env.APP_DB_DATABASE,
+    process.env.APP_DB_USERNAME,
+    process.env.APP_DB_PASSWORD,
+    {
+        host: process.env.APP_DB_HOST,
+        dialect: process.env.APP_DB_CONNECTION,
+        port: process.env.APP_DB_PORT,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 60000,
+            idle: 10000,
+        }
+    }
+);
 
 export default db;
