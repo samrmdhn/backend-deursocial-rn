acce5ba6502c62997481b14fd83855f628f504ae
ivannofick <ivannofick@gmail.com>
2024-11-08 16:06:30 +0700

    fix: fixed seeder location

--- databases/json/scripts/runner.sh
+++ databases/json/scripts/runner.sh
@@ -2,4 +2,4 @@ node databases/json/scripts/regionsCreates.js
 node databases/json/scripts/subRegionsCreates.js
 node databases/json/scripts/countriesCreates.js
 node databases/json/scripts/provincesCreates.js
-node databases/json/scripts/citysCreates.js
+# node databases/json/scripts/citysCreates.js
