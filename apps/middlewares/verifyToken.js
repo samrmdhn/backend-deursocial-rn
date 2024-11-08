10af5b332216dc6f54bf28cbe117bfeb31c0b983
ivannofick <ivannofick@gmail.com>
2024-11-08 16:23:56 +0700

    feat: not used bearer

--- apps/middlewares/verifyToken.js
+++ apps/middlewares/verifyToken.js
@@ -5,26 +5,27 @@ export const verifyToken = (req, res, next) => {
     if (!apiKey || apiKey !== validApiKey) {
         return res.status(403).json({ message: "Forbidden: Invalid API key" });
     }
-    const authHeader = req.headers["authorization"];
-    validationToken(req, res, next, authHeader);
+    const token = req.headers["authorization"]; 
+    validationToken(req, res, next, token);
 };
-const validationToken = (req, res, next, authHeader) => {
-    var fullUrl = req.originalUrl;
-    var agent = req.headers['user-agent']
-    console.log("agent", agent)
+const validationToken = (req, res, next, token) => {
+    const fullUrl = req.originalUrl;
+    const agent = req.headers['user-agent'];
+    console.log("agent", agent);
+
     if (fullUrl === "/api/kadieu") {
         next();
     } else {
-        const token = authHeader && authHeader.split(" ")[1];
-        if (token == null)
-            return res
-                .status(401)
-                .json({ message: "Forbidden: can be access" });
+        if (!token) {
+            return res.status(401).json({ message: "Forbidden: access denied" });
+        }
         jwt.verify(
             token,
             process.env.APP_ACCESS_TOKEN_SECRET,
             (err, decoded) => {
-                if (err) return res.status(403).json({ message: "Forbidden" });
+                if (err) {
+                    return res.status(403).json({ message: "Forbidden" });
+                }
                 next();
             }
         );
