996c0cc78fac9dbf269a2ab57b03aafc755c146c
ivannofick <ivannofick@gmail.com>
2024-11-22 00:04:36 +0700

    fix: fixed RnD websocket

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import http from 'http';
-import dotenv from 'dotenv';
+import http from "http";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import api from "./routes/api.js";
 import fileUpload from "express-fileupload";
@@ -9,34 +9,38 @@ import fileUpload from "express-fileupload";
 import { Server } from "socket.io";
 import { initializeSocket } from "./apps/controllers/ChatGroupsControllers.js";
 
-
 dotenv.config({
-    path: `./.env`
+    path: `./.env`,
 });
 const app = express();
-app.use(cors({
-    credentials: true,
-    origins: [
-        process.env.APP_FRONTEND,
-        "http://localhost:3000"
-    ]
-}));
+app.use(
+    cors({
+        credentials: true,
+        origins: [process.env.APP_FRONTEND, "http://localhost:3000"],
+    })
+);
 
 // app.use(express.json());
 
-app.use(express.json({strict: false}));
+app.use(express.json({ strict: false }));
 app.use(cookieParser());
 app.use(fileUpload());
 app.use(api);
 
-
-
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
-    cors: { origin: '*' },
+    cors: {
+        origins: [
+            process.env.APP_FRONTEND,
+            "http://localhost:3000",
+            "http://localhost:5731",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    },
 });
 initializeSocket(io);
 
 httpServer.listen(process.env.APP_PORT, process.env.APP_HOST, function () {
-    console.log("Started application on port %d", process.env.APP_PORT)
+    console.log("Started application on port %d", process.env.APP_PORT);
 });
