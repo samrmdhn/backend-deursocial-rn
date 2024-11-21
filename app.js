86521ba368003c5a495f0a56295db3106c138f4c
ivannofick <ivannofick.nugraha@mncgroup.com>
2024-11-21 16:23:39 +0700

    feat: Rnd chat

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import api from "./routes/api.js";
 import fileUpload from "express-fileupload";
+// const { Server } = require('socket.io');
+import { Server } from "socket.io";
 
 
 dotenv.config({
@@ -26,7 +28,26 @@ app.use(cookieParser());
 app.use(fileUpload());
 app.use(api);
 
+
+
 const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: { origin: '*' },
+});
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // User bergabung ke grup
+    socket.on('joinGroup', (groupId) => {
+        socket.join(groupId);
+        console.log(`User joined group: ${groupId}`);
+    });
+
+    // Membersihkan listener saat user disconnect
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
 
 httpServer.listen(process.env.APP_PORT, process.env.APP_HOST, function () {
     console.log("Started application on port %d", process.env.APP_PORT)
