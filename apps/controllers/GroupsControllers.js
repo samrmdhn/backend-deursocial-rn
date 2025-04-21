6482554cff7bb82f05370fd87dc6659c2aa9d193
ivannofick <ivannofick@gmail.com>
2025-04-21 20:29:43 +0700

    feat: add group_join_status on getGroupsDetail

--- apps/controllers/GroupsControllers.js
+++ apps/controllers/GroupsControllers.js
@@ -378,6 +378,33 @@ export const getGroupsDetail = async (req, res) => {
                     ) THEN 'waiting approval'
                     ELSE 'not joined'
                 END AS is_joined,
+                json_build_object(
+                    'is_permitted_to_send', 
+                        CASE 
+                            WHEN ${getToken.tod} = 0 THEN false
+                            WHEN u.gender  != g.is_gender THEN false
+                            WHEN u.gender = g.is_gender AND ${getToken.tod} > 0  THEN true
+                            ELSE true
+                        END,
+                    'message_title',
+                        CASE 
+                            WHEN ${getToken.tod} = 0 THEN 'Please log in to join'
+                            WHEN u.gender != g.is_gender THEN  'Sorry, this group is ' ||  CASE  
+                                WHEN g.is_gender = 1 THEN 'male'
+                                WHEN g.is_gender = 2 THEN 'female'
+                                ELSE 'unisex'
+                                END || '-only.'
+                            WHEN u.gender = g.is_gender AND ${getToken.tod} > 0  THEN 'This group is in anonymous mode.'
+                            ELSE ''
+                        END,
+                    'message',
+                        CASE 
+                            WHEN ${getToken.tod} = 0 THEN 'Mohon maaf'
+                            WHEN u.gender  != g.is_gender THEN 'It seems you dont meet the gender requirement for this group.'
+                            WHEN u.gender = g.is_gender AND ${getToken.tod} > 0 THEN 'You will use an anonymous nickname and your profile will be hidden in this group.'
+                            ELSE ''
+                        END
+                ) AS group_join_status,
                 g.description,
                 json_build_object(
                     'is_gender', 
