22a5aadaa20d2d703ca0ac42249c582296ffaa8c
ivannofick <ivannofick@gmail.com>
2025-05-12 19:00:56 +0700

    fix: fixed masih bisa emoji di complete user setelah register

--- apps/validators/usersValidators.js
+++ apps/validators/usersValidators.js
@@ -2,10 +2,24 @@ import Joi from "joi";
 
 export const validationRegisterUsers = (userData) => {
     const userSchema = Joi.object({
-        fullname: Joi.string().required().min(3).max(100),
+        fullname: Joi.string()
+        .required()
+        .min(3)
+        .max(50)
+        .pattern(/^[a-zA-Z0-9]+$/)
+        .messages({
+            "string.pattern.base": "Display name not valid",
+        }),
         description: Joi.string().optional().max(100),
         email: Joi.string().email().required(),
-        username: Joi.string().required().min(3).max(50),
+        username: Joi.string()
+            .required()
+            .min(3)
+            .max(50)
+            .pattern(/^[a-zA-Z0-9._]+$/)
+            .messages({
+                "string.pattern.base": "Username not valid",
+            }),
         gender: Joi.number().required().not(Joi.valid(0)).valid(1, 2).messages({
             "any.required": "Gender is required",
             "any.invalid": "Select Gender",
