9d7346ee7ed5a06f9a246dab7f8e3fd0a8e2c6c8
ivannofick <ivannofick@gmail.com>
2025-06-24 09:50:12 +0700

    fix: fixed text post to moment feat: add feature new post

---
+++ libs/ParseToRichText.js
@@ -0,0 +1,27 @@
+/**
+ * Escape karakter berbahaya agar tidak bisa menyisipkan HTML (mencegah XSS).
+ */
+function escapeHtml(text) {
+    return text
+        .replace(/&/g, '&amp;')   
+        .replace(/</g, '&lt;')    
+        .replace(/>/g, '&gt;')    
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
+/**
+ * Ubah plain caption menjadi rich HTML:
+ * - Ganti newline (\n) menjadi <br>
+ * - Konversi #hashtag dan @mention menjadi link HTML
+ * - Tetap aman dari XSS karena caption di-escape terlebih dahulu
+ */
+export function parseToRichText(caption) {
+    if (!caption || typeof caption !== 'string') return '';
+    let html = escapeHtml(caption);
+    html = html.replace(/#([a-zA-Z0-9_]{1,30})/g, '<a href="/d/$1" class="text-blue-500">#$1</a>');
+    html = html.replace(/@([a-zA-Z0-9_]{1,30})/g, '<a href="/profile/$1" class="text-purple-500">@$1</a>');
+    html = html.replace(/\n/g, '<br>');
+
+    return html;
+}
